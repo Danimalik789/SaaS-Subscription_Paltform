81b94bda6c46ab56adbbf8014f14a2056334689a
Danyal malik <danyalmalik789@gmail.com>
2025-11-21 22:58:59 +0500

    Refactor server.js to improve error handling and logging, and update README with setup instructions.

---
+++ model/Plan.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const planSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  stripePriceId: {
+    type: String,
+  },
+  monthlyLimit: {
+    type: Number,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Plan", planSchema);
